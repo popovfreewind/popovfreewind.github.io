5e8cab2d83d34360542abcb9e18f77def4ffa37e
Dmytro <popovfreewind@gmail.com>
2025-03-04 16:06:04 +0100

    chore: Add initial project structure with images, icons, and configuration files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: './src/index.js',
+        mode: argv.mode,
+        devtool: 'source-map',
+        devServer: {
+            static: path.resolve(__dirname, 'dist'),
+            port: 3000,
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                }
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: './src/index.html' }),
+        ],
+    }
+};
