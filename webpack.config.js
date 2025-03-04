8e8584591fc4e3ed7041b68422e184fbc4189c74
Dmytro <popovfreewind@gmail.com>
2025-03-04 16:19:05 +0100

    chore: Add new images and icons for project assets; update import path for CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
     const isProduction = argv.mode === 'production';
@@ -25,6 +26,11 @@ module.exports = (env, argv) => {
         },
         plugins: [
             new HtmlWebpackPlugin({ template: './src/index.html' }),
+            new CopyPlugin({
+                patterns: [
+                  { from: "./src/assets", to: "assets" },
+                ],
+              }),
         ],
     }
 };
